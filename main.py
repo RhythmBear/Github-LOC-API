5803b6bd71a327b77defbea08e7ae1e9514a8c18
EMMANUEL <femiemmauel1990@gmail.com>
2022-12-26 19:35:04 +0100

    Added Database/Date feature

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import requests
+import pandas as pd
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from DateTime.DateTime import datetime as dt
+
+#universal format for date = 'YYYY-MM-DD'
 
 
 # This is my API to get the total lines of code in my github repository
 
 LOC_URL = "https://api.codetabs.com/v1/loc"
 # USER = "RhythmBear"
+
+# ------------------- FUNCTIONS -----------------------------------------------------------
+
+
+def confirm_date(last_date: str):
+    """
+    This function takes in a date input in the format yyyy-mm-dd and returns the number of days
+    between the given date and the current date.
+    :param last_date:
+    :return:
+    """
+    today_date = dt.now().date()
+    # print(f"today's date is {today_date}")
+    previous_date = dt(int(last_date.split('-')[0]), int(last_date.split('-')[1]), int(last_date.split('-')[2])).date()
+    days_between = today_date - previous_date
+    days_between = days_between.days
+    # print(f"There are {days_between} days between {today_date} and {previous_date}. ")
+
+    return days_between
 
 
 def get_lines_for_repo(github_account: str, repo_name: str):
@@ -29,8 +53,37 @@ def get_lines_for_repo(github_account: str, repo_name: str):
     return lines_of_code
 
 
+def create_table():
+    data = {'Username': [],
+            'Lines Of Code': [],
+            'Date Checked': []
+            }
+
+def update_table(name, code_lines, date):
+    data = {'Username': name,
+            'Lines Of Code': code_lines,
+            'Date Checked': date}
+
+
+# ---------------------- DEFINING ROUTES ------------------------------------------------------------
+
 # Flask API_KEY
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///loc_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKrfb'
+# Create database
+db = SQLAlchemy(app)
+
+
+class GithubLines(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    lines_of_code = db.Column(db.Integer, nullable=True, unique=False)
+    date_updated = db.Column(db.String(25), nullable=False)
+
+#
+db.create_all()
 
 @app.route("/")
 def home():
@@ -43,11 +96,8 @@ def home():
 
 @app.route("/<user>")
 def get_lines(user):
-
     print(request.args)
-
     # Get the names of all the repositories in a github account.
-
     repo_url = f"https://api.github.com/users/{user}/repos"
 
     repo_response = requests.get(repo_url)
@@ -71,9 +121,13 @@ def get_lines(user):
     results = {}
     total_lines = 0
     for item in repo_response.json():
-        repository_name = item['name']
-        lines_result = get_lines_for_repo(user, repository_name)
-        total_lines += lines_result
+        # Check to make sure we are not getting forked repositories
+        if item['fork']:
+            continue
+        else:
+            repository_name = item['name']
+            lines_result = get_lines_for_repo(user, repository_name)
+            total_lines += lines_result
 
         # Append the result to result dictionary
         results[repository_name] = lines_result
---
+++ test.py
@@ -0,0 +1,3 @@
+from main import confirm_date
+
+print(confirm_date('2022-11-2'))
